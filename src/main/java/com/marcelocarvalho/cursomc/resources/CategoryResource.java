85fd20a7ed81b3aa06eeae4c0edcee96aedda0b5
Marcelo Carvalho <marcelolcarvalho@gmail.com>
2019-06-24 13:04:19 -0300

    Testing first class domain - Category

--- src/main/java/com/marcelocarvalho/cursomc/resources/CategoryResource.java
+++ src/main/java/com/marcelocarvalho/cursomc/resources/CategoryResource.java
@@ -1,16 +1,33 @@
 package com.marcelocarvalho.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcelocarvalho.cursomc.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "Rest is working !!!";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Info");
+		
+		Category cat2 =  new Category(2, "Office");
+		
+		List<Category> listCat = new ArrayList<>();
+		
+		listCat.add(cat1);
+		listCat.add(cat2);
+		
+		return listCat;
 	}
+	
+	
 }
