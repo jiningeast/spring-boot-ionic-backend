40179dc8b790fcc3dc138e26b9b9f00b67c6e4d7
Marcelo Carvalho <marcelolcarvalho@gmail.com>
2019-07-03 06:10:16 -0300

    H2 database

--- src/main/java/com/marcelocarvalho/cursomc/domain/Category.java
+++ src/main/java/com/marcelocarvalho/cursomc/domain/Category.java
@@ -2,66 +2,70 @@ package com.marcelocarvalho.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category implements Serializable {
 
-
 	private static final long serialVersionUID = 1L;
-	
-	
-		private Integer id;
-		private String name;
-		
-		public Category() {
-			
-		}
 
-		public Category(Integer id, String name) {
-			super();
-			this.id = id;
-			this.name = name;
-		}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
 
-		public Integer getId() {
-			return id;
-		}
+	public Category() {
 
-		public void setId(Integer id) {
-			this.id = id;
-		}
+	}
 
-		public String getName() {
-			return name;
-		}
+	public Category(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
 
-		public void setName(String name) {
-			this.name = name;
-		}
+	public Integer getId() {
+		return id;
+	}
 
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((id == null) ? 0 : id.hashCode());
-			return result;
-		}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Category other = (Category) obj;
-			if (id == null) {
-				if (other.id != null)
-					return false;
-			} else if (!id.equals(other.id))
-				return false;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
 			return true;
-		}
-		
-		
-	
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
