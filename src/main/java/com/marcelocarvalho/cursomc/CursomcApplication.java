87d7a7e157001272201337ebefd16e6957f97538
Marcelo Carvalho <marcelolcarvalho@gmail.com>
2019-07-03 23:00:09 -0300

    instantiate Category

--- src/main/java/com/marcelocarvalho/cursomc/CursomcApplication.java
+++ src/main/java/com/marcelocarvalho/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.marcelocarvalho.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.marcelocarvalho.cursomc.domain.Category;
+import com.marcelocarvalho.cursomc.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "Computer");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+
 
 }
--- src/main/java/com/marcelocarvalho/cursomc/services/CategoryService.java
+++ src/main/java/com/marcelocarvalho/cursomc/services/CategoryService.java
@@ -12,10 +12,10 @@ import com.marcelocarvalho.cursomc.repositories.CategoryRepository;
 public class CategoryService {
 
 	@Autowired
-	private CategoryRepository cateoryRepository;
+	private CategoryRepository categoryRepository;
 	
 	public Category find(Integer id) {
-		Optional<Category> obj = cateoryRepository.findById(id); 	
+		Optional<Category> obj = categoryRepository.findById(id); 	
 		return obj.orElse(null) ;
 	}
 	
